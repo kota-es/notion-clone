3c8843782bface42b6bc05cbcfc140e87ec355af
kota-es <info@k-3110.com>
2022-10-09 17:36:14 +0900

    construct backend

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
-const PORT = 6000;
+const PORT = 5000;
+require('dotenv').config()
+
+// DB接続
+try {
+  mongoose.connect(process.env.MONGODB_URL)
+  console.log('DBと接続中。。。')
+} catch (error) {
+  console.log(error)
+}
 
 app.listen(PORT, () => {
   console.log('ローカルサーバー起動中。。。')
