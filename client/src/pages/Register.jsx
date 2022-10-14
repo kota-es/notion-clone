c8e8a7391ea591f1d35328a5b2634a52877ffc2b
kota-es <info@k-3110.com>
2022-10-14 22:22:23 +0900

    create get register input

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -4,9 +4,23 @@ import { LoadingButton } from "@mui/lab";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    // 入力欄の文字列を取得
+    const data = new FormData(e.target)
+    const username = data.get('username').trim()
+    const password = data.get('password').trim()
+    const confirmPassword = data.get('confirmPassword').trim()
+    console.log(username)
+    console.log(password)
+    console.log(confirmPassword)
+  }
+
+
   return(
     <>
-      <Box component="form">
+      <Box component="form" onSubmit={handleSubmit}>
         <TextField fullWidth id="username" label="お名前" margin="normal" name="username" required/>
         <TextField fullWidth id="password" label="パスワード" margin="normal" name="password"  type="password" required/>
         <TextField fullWidth id="confirmPassword" label="確認用パスワード" margin="normal" name="confirmPassword"  type="password" required/>
