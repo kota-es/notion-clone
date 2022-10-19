288593ee96675024fd11497bf019f674dc3074f7
kota-es <info@k-3110.com>
2022-10-19 21:53:24 +0900

    create AppLayout

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,36 @@
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import AuthLayout from './components/layout/AuthLayout';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { CssBaseline } from '@mui/material';
-import { blue } from '@mui/material/colors'
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AuthLayout from "./components/layout/AuthLayout";
+import AppLayout from "./components/layout/AppLayout";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { blue } from "@mui/material/colors";
 
 function App() {
-
   const theme = createTheme({
-    palette: { primary: blue},
+    palette: { primary: blue },
   });
 
-  return(
+  return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<AuthLayout />}>
-            <Route path='login' element={<Login />} />
-            <Route path='register' element={<Register />} />
+          <Route path="/" element={<AuthLayout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Home />} />
+            <Route path="memo" element={<Home />} />
           </Route>
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
-  )
+  );
 }
 
 export default App;
