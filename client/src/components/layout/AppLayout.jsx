a8422e925d3d165cc3cb072ad7cf961acc3853fa
kota-es <info@k-3110.com>
2022-10-20 22:49:33 +0900

    create AppLayout

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router";
+import Box from "@mui/material/Box";
+import authUtils from "../../utils/authUtils";
+import Sidebar from "../common/Sidebar";
 
 const AppLayout = () => {
-  return <div>AppLayout</div>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // JWTを持っているのか確認する
+    const checkAuth = async () => {
+      // 認証チェック
+      const user = await authUtils.isAuthenticated();
+      if (!user) {
+        navigate("/login");
+      }
+    };
+    checkAuth();
+  }, [navigate]);
+
+  return (
+    <div>
+      <Box sx={{ display: "flex" }}>
+        <Sidebar />
+        <Box sx={{ flexGrow: 1, p: 1, width: "max-content" }}>
+          <Outlet />
+        </Box>
+      </Box>
+    </div>
+  );
 };
 
 export default AppLayout;
